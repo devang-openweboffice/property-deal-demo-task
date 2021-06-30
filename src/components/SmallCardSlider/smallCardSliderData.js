65a20b5a6a6e61efaa5b3c70d78e37ce82b9e126
Devang Pujara <62253858+devang-openweboffice@users.noreply.github.com>
2021-06-30 14:32:57 +0530

    Update smallCardSliderData.js

--- src/components/SmallCardSlider/smallCardSliderData.js
+++ src/components/SmallCardSlider/smallCardSliderData.js
@@ -1,130 +1,57 @@
-import {
-  faUsers,
-  faGavel,
-  faChartArea,
-  faDollarSign,
-  faBlind,
-  faGraduationCap,
-  faCreditCard,
-  faUniversity,
-  faPercent,
-  faTag,
-  faBullhorn,
-  faCalendarCheck,
-  faTrophy,
-  faTruck,
-  faNewspaper,
-  faBuilding,
-  faTrain,
-} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import { topCardSliderData, bottomCardSliderData } from "./smallCardSliderData";
+import "./smallCardSlider.scss";
+import Marquee from "react-fast-marquee";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export const topCardSliderData = [
-  {
-    icon: faUsers,
-    title: "Population growth trend", 
-    bgClass: "yellow-bg"
-  },
-  {
-    icon: faGavel,
-    title: "Crime rate", 
-    bgClass: "orange-bg"
-  },
-  {
-    icon: faChartArea,
-    title: "Price per sq. ft", 
-    bgClass: "blue-bg"
-  },
-  {
-    icon: faDollarSign,
-    title: "Sold price", 
-    bgClass: "purple-bg"
-  },
-  {
-    icon: faBlind,
-    title: "Population age groups", 
-    bgClass: "yellow-bg"
-  },
-  {
-    icon: faGraduationCap,
-    title: "Education", 
-    bgClass: "orange-bg"
-  },
-  {
-    icon: faCreditCard,
-    title: "Economic activity rate", 
-    bgClass: "blue-bg"
-  },
-  {
-    icon: faUsers,
-    title: "Occupational grade", 
-    bgClass: "purple-bg"
-  },
-  {
-    icon: faUniversity,
-    title: "Outstanding mortgages", 
-    bgClass: "yellow-bg"
-  },
-  {
-    icon: faUsers,
-    title: "Average asking price", 
-    bgClass: "orange-bg"
-  },
-];
 
-export const bottomCardSliderData = [
-  {
-    icon: faPercent,
-    title: "Average yield", 
-    bgClass: "orange-bg"
-  },
-  {
-    icon: faTag,
-    title: "New built ratio", 
-    bgClass: "blue-bg"
-  },
-  {
-    icon: faBullhorn,
-    title: "Asking price changes", 
-    bgClass: "purple-bg"
-  },
-  {
-    icon: faCalendarCheck,
-    title: "Average time to sell", 
-    bgClass: "yellow-bg"
-  },
-  {
-    icon: faTruck,
-    title: "Time to rent", 
-    bgClass: "orange-bg"
-  },
-  {
-    icon: faTrophy,
-    title: "Return-on-investment", 
-    bgClass: "blue-bg"
-  },
-  {
-    icon: faNewspaper,
-    title: "Estate agent", 
-    bgClass: "purple-bg"
-  },
-  {
-    icon: faBuilding,
-    title: "Property type", 
-    bgClass: "yellow-bg"
-  },
-  {
-    icon: faUsers,
-    title: "Schools nearby", 
-    bgClass: "orange-bg"
-  },
-  {
-    icon: faTrain,
-    title: "Distance to train stations", 
-    bgClass: "blue-bg"
-  },
-  {
-    icon: faUsers,
-    title: "Restaurants & cafes", 
-    bgClass: "yellow-bg"
-  },
-]
+const SmallCardSlider = () => {
+  var settings = {
+    speed: 35,
+    delay: 0,
+    gradient: false,
+  };
+
+  return (
+    <>
+      <div className="data__rely__section">
+        <div className="container">
+          <h2>
+            Data you <span>can rely on</span>
+          </h2>
+        </div>
+
+        <div className="data__rely__slider">
+          <div className="data__rely__slider__top">
+            <Marquee {...settings} speed={50}>
+              {topCardSliderData &&
+                topCardSliderData.map((slide, i) => (
+                  <div className="single__slide" key={`slide-${i}`}>
+                    <div className={`circle ${slide.bgClass}`}>
+                      <FontAwesomeIcon icon={slide.icon} size="1x" />
+                    </div>
+                    <p>{slide.title}</p>
+                  </div>
+                ))}
+            </Marquee>
+          </div>
+          <div className="data__rely__slider__bottom">
+            <Marquee {...settings} direction="right">
+              {bottomCardSliderData &&
+                bottomCardSliderData.map((slide, i) => (
+                  <div className="single__slide" key={`slide-${i}`}>
+                    <div className={`circle ${slide.bgClass}`}>
+                      <FontAwesomeIcon icon={slide.icon} size="1x" />
+                    </div>
+                    <p>{slide.title}</p>
+                  </div>
+                ))}
+            </Marquee>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SmallCardSlider;
